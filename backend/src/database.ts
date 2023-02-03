76f13ed48d98637682d4d5ebbbb59f2b8b15bef4
Seif Ashraf <seif_fire2@hotmail.com>
2023-02-03 17:32:04 +0200

    initial

---
+++ backend/src/database.ts
@@ -0,0 +1,21 @@
+import { Pool } from 'pg';
+
+const { POSTGRES_HOST, POSTGRES_DB, POSTGRES_DB_TEST, POSTGRES_USER, POSTGRES_PASSWORD, ENV } =
+    process.env;
+
+const client: Pool =
+    ENV === 'dev'
+        ? new Pool({
+              database: POSTGRES_DB,
+              host: POSTGRES_HOST,
+              user: POSTGRES_USER,
+              password: POSTGRES_PASSWORD,
+          })
+        : new Pool({
+              database: POSTGRES_DB_TEST,
+              host: POSTGRES_HOST,
+              user: POSTGRES_USER,
+              password: POSTGRES_PASSWORD,
+          });
+
+export default client;
