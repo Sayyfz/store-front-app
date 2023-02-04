cc5a3471912d6b9186c737f3f56f0cd379dee6af
Seif Ashraf <seif_fire2@hotmail.com>
2023-02-04 20:13:43 +0200

    orders model, controller and routes implemented, but all tests should get refactored

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import usersRoute from './routes/usersRoute';
 import productsRoute from './routes/productsRoute';
+import ordersRoute from './routes/ordersRoute';
 
 const app: express.Application = express();
 const port = process.env.PORT || 5000;
@@ -9,6 +10,7 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use('/users', usersRoute);
 app.use('/products', productsRoute);
+app.use('/orders', ordersRoute);
 
 app.get('/', function (req: Request, res: Response) {
     res.send('Welcome, you can check for the endpoints in the README.md file');
--- backend/src/tests/route_tests/ordersRouteSpec.ts
+++ backend/src/tests/route_tests/ordersRouteSpec.ts
@@ -1,1 +1,55 @@
 import fetcher from '../indexSpec';
+import { Order } from '../../models/order';
+import supertest from 'supertest';
+
+describe('ORDERS ROUTE SPEC', () => {
+    beforeAll(done => {
+        fetcher
+            .post('/users')
+            .send({ first_name: 'joe', last_name: 'khalid', password: 'password' })
+            .end((err, res: supertest.Response) => {
+                if (err) console.log(err);
+                done();
+            });
+    });
+
+    it('GET /orders should return json', (done: DoneFn) => {
+        fetcher
+            .get('/orders')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .end((err, res: supertest.Response) => {
+                if (err) console.log(err);
+                done();
+            });
+    });
+
+    it('Post request to /orders should create an order successfully', done => {
+        fetcher
+            .post('/orders')
+            .send({ user_id: '1', status: 'active' })
+            .expect('Content-Type', /json/)
+            .end((err, res: supertest.Response) => {
+                if (err) console.log(err);
+
+                expect(res.body).toEqual({ id: 1, user_id: 1, status: 'active' });
+
+                done();
+            });
+    });
+
+    it('GET request to /orders/1 should show the order that has id of 1', done => {
+        fetcher
+            .get('/orders/1')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .end((err, res: supertest.Response) => {
+                if (err) console.log(err);
+
+                expect(res.body).toEqual({ id: 1, user_id: 1, status: 'active' });
+                done();
+            });
+    });
+});
+
+//TODO: MIGRATION DOWN DOESNT RUN AFTER TEST ___________________________________________________________
