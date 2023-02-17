5177ed986cdd59e78894345f013801e198340c26
Sayyfz <seif_fire2@hotmail.com>
2023-02-17 18:11:23 +0200

    s

--- backend/src/routes/dashboardRoute.ts
+++ backend/src/routes/dashboardRoute.ts
@@ -8,7 +8,7 @@ import { verifyToken } from '../middlewares/verifyToken';
 const dashboardRoutes = express.Router();
 
 dashboardRoutes.get('/current_orders_by_user/:id', verifyToken, current_orders_by_user);
-dashboardRoutes.get('/completed_orders_by_user/:id', verifyToken, completed_orders_by_user);
+// dashboardRoutes.get('/completed_orders_by_user/:id', verifyToken, completed_orders_by_user);
 dashboardRoutes.get('/products_by_category/:category', products_by_category);
 
 export default dashboardRoutes;
--- backend/src/routes/usersRoute.ts
+++ backend/src/routes/usersRoute.ts
@@ -3,7 +3,7 @@ import { authenticate, create, index, show } from '../controllers/usersControlle
 import { verifyToken } from '../middlewares/verifyToken';
 const usersRoute = express.Router();
 
-usersRoute.get('/', verifyToken, index);
+// usersRoute.get('/', verifyToken, index);
 usersRoute.post('/', create);
 usersRoute.get('/:id', verifyToken, show);
 usersRoute.post('/authenticate', authenticate);
