e32d4e6b551c160782d52ef53fc2c6fb6302becc
Sayyfz <seif_fire2@hotmail.com>
2023-03-06 23:29:47 +0200

    feat: redux added to project

--- store-front-client/src/App.tsx
+++ store-front-client/src/App.tsx
@@ -1,10 +1,31 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
+import React, { useState } from 'react';
+import { useAppDispatch, useAppSelector } from './app/hooks';
+import { incremented, amountAdded } from './slices/counter-slice';
 
 function App() {
-    const [count, setCount] = useState(0);
+    const count = useAppSelector(state => state.counter.value);
+    const dispatch = useAppDispatch();
 
-    return <div className='App'></div>;
+    const handleClick = () => {
+        dispatch(amountAdded(3));
+    };
+    return (
+        <div className='App'>
+            <div>
+                <a href='https://vitejs.dev' target='_blank'>
+                    <img src='/vite.svg' className='logo' alt='Vite logo' />
+                </a>
+            </div>
+            <h1>Vite + React</h1>
+            <div className='card'>
+                <button onClick={handleClick}>count is {count}</button>
+                <p>
+                    Edit <code>src/App.tsx</code> and save to test HMR
+                </p>
+            </div>
+            <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
+        </div>
+    );
 }
 
 export default App;
--- store-front-client/src/main.tsx
+++ store-front-client/src/main.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/App.scss';
 import './styles/App.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <React.StrictMode>
+    <Provider store={store}>
         <App />
-    </React.StrictMode>,
+    </Provider>,
 );
